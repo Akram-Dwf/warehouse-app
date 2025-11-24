f67ed638379e97da76646cdb7c15c72aebdca920
Akram-Dwf <akramtamir12@gmail.com>
2025-11-24 22:54:39 +0800

    feat: implement UserSeeder for initial accounts

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // 1. Akun Admin (Akses Penuh)
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@gudang.com',
+            'role' => 'admin',
+            'password' => Hash::make('password'),
+        ]);
+
+        // 2. Akun Warehouse Manager (Kepala Gudang)
+        User::create([
+            'name' => 'Manager Gudang',
+            'email' => 'manager@gudang.com',
+            'role' => 'manager',
+            'password' => Hash::make('password'),
+        ]);
+
+        // 3. Akun Staff Gudang (Pekerja)
+        User::create([
+            'name' => 'Staff Gudang 1',
+            'email' => 'staff@gudang.com',
+            'role' => 'staff',
+            'password' => Hash::make('password'),
+        ]);
+
+        // 4. Akun Supplier (Contoh Mitra)
+        User::create([
+            'name' => 'PT Supplier Jaya',
+            'email' => 'supplier@pt.com',
+            'role' => 'supplier',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
